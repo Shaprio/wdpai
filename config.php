85d6073263be74ee488e1b844a1dbba10ef6e5cf
Rafał Szczyrbak <1skorcz24@gmail.com>
2023-06-28 10:10:06 +0200

    Database

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const USERNAME = 'abdirvzpihbhnz';
+const PASSWORD = 'd80a4d5f91aae862c685b21a970461d2f1e223721d40b7ba700402d58f21082f';
+const HOST = 'ec2-34-242-199-141.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'd5skgnh92gc1hq';
